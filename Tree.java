d0ec65e4f4eb89caeb52660320ef1e5a366c37c9
Adam Kelso <61440423+Kelsoadam@users.noreply.github.com>
2021-03-30 11:14:27 +0100

    Create Tree.java
    File created and code copied from finished project

---
+++ Tree.java
@@ -0,0 +1,18 @@
+// Define your tree class in this file!
+
+public class Tree {
+  public static void main(String[] args) {
+    /*
+    Code for main method where we will be introducing ourselves and  planting a tree to the screen
+    */
+
+  System.out.println("Hi I'm Adam!");
+  System.out.println("Today I will be planting a tree!");
+
+    System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println(" *** ");
+    System.out.println("  *  ");
+    System.out.println("  *  ");
+  }
+}
